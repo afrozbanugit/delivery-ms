6f715b158bea4d066b96fdd2d00a55af8c0fe62a
afrozbanu <afrozjavasession7@gmail.com>
2025-06-17 02:39:14 -0700

    Changed Controller to RestController

--- src/main/java/com/delivery/ms/controller/DeliveryController.java
+++ src/main/java/com/delivery/ms/controller/DeliveryController.java
@@ -5,7 +5,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 
 import org.springframework.stereotype.Controller;
-
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,7 +17,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class DeliveryController {
 
